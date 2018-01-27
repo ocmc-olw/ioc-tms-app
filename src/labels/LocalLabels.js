7b76781a5c8fcd021599f7cac201563f697996de
Michael Colburn <mac002@theColburns.us>
2018-01-27 16:16:34 -0500

    1.7.2

--- src/labels/LocalLabels.js
+++ src/labels/LocalLabels.js
@@ -51,6 +51,29 @@ const   labels = {
       , itemOntology: "Ontology"
       , itemText: "Texts"
     }
+    , about: {
+      title: "About the TMS"
+      , p1: "This Translation Management System (TMS) was developed by and is maintained by staff of the Orthodox Christian Mission Center. It is Pan-Orthodox, available for all canonical jurisdictions of the Eastern Orthodox Christian Church world-wide."
+      , p2: "The purpose of the TMS is provide an environment for people to create, manage, and publish translations of the Eastern Orthodox Christian liturgical texts.  It can also be used for other types of material such as daily readings, educational, or catechetical texts."
+      , userFeaturesTitle: "TMS Features for Translators"
+      , userFeaturesIntro: "The TMS provides translators the ability to..."
+      , userFeature01: "Create or paste in their own translation while viewing the Greek source text and translations of the text."
+      , userFeature02: "View grammatical information about the Greek text."
+      , userFeature03: "View notes made by scholars and other translators."
+      , userFeature04: "Submit a translation for review and/or approval."
+      , userFeature05: "Produce and download a PDF for a book or service, with one, two, or three translations side-by-side."
+      , userFeature06: "Produce notes for personal use, which can only be viewed by that user unless he or she wants to share them with others."
+      , securityFeaturesTitle: "Security Features of the TMS"
+      , securityFeaturesIntro: "The TMS provides a variety of security features:"
+      , securityFeature01: "User accounts are created by administrators who have verified the identity of the user."
+      , securityFeature02: "Access to the TMS requires entry of a valid username and password."
+      , securityFeature03: "All records in the database are grouped into libraries.  Authorization to access records in a library must be granted to a user by an administrator for that library.  There are types of access (also called 'roles') that can be granted. They are: reader, author, reviewer, and administrator.  A reader may only read a database record.  A reader cannot update a record.  An author can create and update records.  A reviewer can create and update comments about a translation or other text for the purpose of reviewing it.  An administrator can grant other users the role of reader, author, reviewer, or administrator.  The administrator must have administrator rights for that library."
+      , securityFeature04: "All records have a status field: Editing, Holding, Ready to Review, Reviewing, Ready to Edit, Ready to Finalize, Finalized, Ready to Release, or Released.  These status control the life-cycle of a record and the workflow for managing a record."
+      , securityFeature05: "All records have a visibility field: Personal, Private, or Public.  If the visibility is 'Personal', only the library owner can see or edit it.  If the visibility is 'Private', only those granted access can see or edit it.  If the visibility is 'Public', anyone can see or view it."
+      , securityFeature06: "When a record's status is 'Released', it is automatically copied to a separate read-only database. If the visibility is 'Public', anyone can view it in that database."
+      , securityFeature07: "When a record is created or updated, the record contains the username and the date and time at which the record was created and/or updated.  It is possible to determine each step of the way, who changed what and when."
+      , securityFeature08: "Multiple backups are periodically made from each database.  Access to the backups is secured."
+    }
   }
     , el: {
     paratexts: {
@@ -100,6 +123,29 @@ const   labels = {
       , itemOntology: "Οντολογία"
       , itemText: "κείμενα"
     }
+    , about: {
+      title: "Σχετικά με το TMS"
+      , p1: "This Translation Management System (TMS) was developed by and is maintained by staff of the Orthodox Christian Mission Center."
+      , p2: "The purpose of the TMS is provide an environment for people to create, manage, and publish translations of the Eastern Orthodox Christian liturgical texts.  It can also be used for other types of material such as daily readings, educational, or catechetical texts."
+      , userFeaturesTitle: "Features for Users of the TMS"
+      , userFeaturesIntro: "The TMS provides users the ability to..."
+      , userFeature01: "Create or paste in their own translation while view the Greek source text and translations of the text."
+      , userFeature02: "View grammatical information about the Greek text."
+      , userFeature03: "View notes made by scholars and other translators."
+      , userFeature04: "Submit a translation for review and/or approval."
+      , userFeature05: "Produce and download a PDF for a book or service, with one, two, or three translations side-by-side."
+      , userFeature06: "Produce notes for personal use, which can only be viewed by that user unless he or she wants to share them with others."
+      , securityFeaturesTitle: "Security Features of the TMS"
+      , securityFeaturesIntro: "The TMS provides a variety of security features:"
+      , securityFeature01: "User accounts are created by administrators who can personally verify the identity of the user."
+      , securityFeature02: "Access to the TMS requires entry of a valid username and password."
+      , securityFeature03: "All records in the database are grouped into libraries.  Authorization to access records in a library must be granted to a user by an administrator for that library.  There are three types of access that can be granted: reader, author, reviewer, administrator.  A reader may only read a database record.  A reader cannot update a record.  An author can create and updated records.  A reviewer can crate and update comments about a translation or other text for the purpose of reviewing it.  An administrator can grant other users the role of reader, author, reviewer, or administrator.  The administrator must have administrator rights for that library."
+      , securityFeature04: "All records have a status field: Editing, Holding, Ready to Review, Reviewing, Ready to Edit, Ready to Finalize, Finalized, Ready to Release, Released.  These status control the life-cycle of a record and the workflow for managing a record."
+      , securityFeature05: "All records have a visibility field: Personal, Private, or Public.  If the visibility is 'Personal', only the library owner can see or edit it.  If the visibility is 'Private', only those granted access can see or edit it.  If the visibility is 'Public', any one can see or view it."
+      , securityFeature06: "When a record's status is 'Released', it is automatically copied to a separate read-only database. If the visibility is 'Public', anyone can view it in that database."
+      , securityFeature07: "When a record is created or updated, the record contains the username and the date and time at which the record was created and/or updated."
+      , securityFeature08: "Multiple backups are periodically made from each database.  Access to the backups is secured."
+    }
   }
 }
 
@@ -108,4 +154,7 @@ module.exports = {
   , getAllLabels: (code) => {
     return labels[code];
     }
+  , getAboutTmsLabels: (code) => {
+    return labels[code].about;
+  }
 }
--- src/modules/pages/About.js
+++ src/modules/pages/About.js
@@ -4,12 +4,63 @@ import Email from "../components/images/SsEmailContact"
 import {AboutDatabase, Configuration} from 'ioc-liturgical-react'
 import VersionNumbers from "../../config/VersionNumbers";
 import { connect } from 'react-redux';
+import LocalLabels from '../../labels/LocalLabels';
+import { Jumbotron, PageHeader } from 'react-bootstrap';
 
 class About extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      labels: {
+        about: LocalLabels.getAboutTmsLabels(props.app.session.languageCode)
+      }
+    }
+  }
+  componentWillReceiveProps = (nextProps) => {
+    this.setState(
+        {
+          labels: {
+            about: LocalLabels.getAboutTmsLabels(nextProps.app.session.languageCode)
+          }
+        }
+    )
+  }
+
+    render() {
     const version = VersionNumbers.getPackageNumber();
     return (
         <div className="App-page App-page-about">
+          <PageHeader>{this.state.labels.about.title}</PageHeader>
+          <Jumbotron>
+            <p>{this.state.labels.about.p1}</p>
+            <p>{this.state.labels.about.p2}</p>
+          </Jumbotron>
+          <PageHeader>{this.state.labels.about.userFeaturesTitle}</PageHeader>
+          <Jumbotron>
+            <p>{this.state.labels.about.userFeaturesIntro}</p>
+            <ol>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature01}</li>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature02}</li>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature03}</li>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature04}</li>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature05}</li>
+              <li className="App-about-list-item">{this.state.labels.about.userFeature06}</li>
+            </ol>
+          </Jumbotron>
+          <PageHeader>{this.state.labels.about.securityFeaturesTitle}</PageHeader>
+          <Jumbotron>
+            <p>{this.state.labels.about.securityFeaturesIntro}</p>
+            <ol>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature01}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature02}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature03}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature04}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature05}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature06}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature07}</li>
+              <li className="App-about-list-item">{this.state.labels.about.securityFeature08}</li>
+            </ol>
+          </Jumbotron>
           <AboutDatabase labels={this.props.app.session.labels.pageAbout}/>
           {this.props.app.session.labels.pageAbout.contact} <Email />
           <p/>
