34e5a0f882027c80af5ade702ec20484bec04085
Michael Colburn <mac002@theColburns.us>
2018-03-10 10:21:55 -0500

    v1.8.6

--- src/config/VersionNumbers.js
+++ src/config/VersionNumbers.js
@@ -5,7 +5,7 @@
 
 // because of the way the install bash script works
 // the last number needs to be 9 or less
-const packageNumber = "1.8.5";
+const packageNumber = "1.8.6";
 
 module.exports = {
   getPackageNumber: () => { return packageNumber;}
