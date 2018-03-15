519cb68cd6af2ee740e5db7801c6fd92ba3b432b
Michael Colburn <mac002@theColburns.us>
2018-03-15 15:56:31 -0400

    1.9.9

--- src/modules/pages/Help.js
+++ src/modules/pages/Help.js
@@ -4,13 +4,41 @@
 import React from 'react';
 import {HelpSearch} from 'ioc-liturgical-react'
 import { connect } from 'react-redux';
+import { Jumbotron } from 'react-bootstrap';
 
 export class Help extends React.Component {
   render() {
     return (
-        <div className="App-page App-help">
-          <HelpSearch labels={this.props.app.session.labels.help.search}/>
-        </div>
+      <div className="App-page App-help">
+        <h3>Videos</h3>
+        <Jumbotron>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=dwtervZaeQo"} target={"_blank"}>Logon / Logoff</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=0_kw1VYY2i4"} target={"_blank"}>Overview of Menu Items</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=o2IZx4JE4y4"} target={"_blank"}>Changing the Language of the User Interface</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=WM6aPV6cNZg"} target={"_blank"}>Selecting a Service or Liturgical Book from AGES DCS</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=K6izGk5hJVs"} target={"_blank"}>Generating a Service or Liturgical Book</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=6aqw7MJhMx8"} target={"_blank"}>Entering a Translation</a>
+            </p>
+            <p>
+              <a href={"http://www.youtube.com/watch?v=Q60boh0Toq8"} target={"_blank"}>Creating / Editing Notes about a Text</a>
+            </p>
+          <p>
+            <a href={"http://www.youtube.com/watch?v=INabNd1RHG4"} target={"_blank"}>Searching Notes About a Text</a>
+          </p>
+        </Jumbotron>
+        <HelpSearch labels={this.props.app.session.labels.help.search}/>
+      </div>
     )
   }
 }
