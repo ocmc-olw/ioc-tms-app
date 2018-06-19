46af8571f5055ecf1aee4c03f6832007be7b4e49
Michael Colburn <mac002@theColburns.us>
2018-06-19 17:41:51 -0400

    2018.06.19.5

--- src/config/VersionNumbers.js
+++ src/config/VersionNumbers.js
@@ -5,7 +5,7 @@
 
 // because of the way the install bash script works
 // the last number needs to be 9 or less
-const packageNumber = "2018.06.19.4";
+const packageNumber = "2018.06.19.5";
 
 module.exports = {
   getPackageNumber: () => { return packageNumber;}
